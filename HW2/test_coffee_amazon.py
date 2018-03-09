e7a95e8a6b7c7fe96059d9f41b86f6308bc4ef83
David Porter <davidbrianporter@gmail.com>
2018-03-09 17:09:17 -0500

    copied coffee example need to get selenium working locally

---
+++ HW2/test_coffee_amazon.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+browser = None
+
+def setup_module(module):
+    global browser
+    browser = webdriver.Chrome()
+    browser.get("http://www.amazon.com")
+
+def teardown_module(module):
+    time.sleep(10)
+    if browser:
+        browser.close()
+
+def test_did_we_get_to_amazon():
+    assert "Amazon" in browser.title
+
+def test_is_cuisinart_a_displayed_coffee_maker():
+    id = "twotabsearchtextbox"
+    searchbox = browser.find_element_by_id(id)
+    searchbox.clear()
+    searchbox.send_keys("coffee maker")
+    searchbox.send_keys(Keys.RETURN)
+    time.sleep(10)
+    id="s-results-list-atf"
+    result_list = browser.find_element_by_id(id)
+    result_text = result_list.text
+    assert "Cuisinart" in result_text
