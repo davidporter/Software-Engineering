34c1b21816422204d7c739052d3442b57f76b5b2
David Porter <davidbrianporter@gmail.com>
2018-03-09 17:03:11 -0500

    I got the API test to work - kinda kludgy

--- HW2/test_api.py
+++ HW2/test_api.py
@@ -1,3 +1,13 @@
 import requests
+import json
 
-r = requests.get('https://api.github.com/events')
+r = requests.get('http://routingnumbers.herokuapp.com/api/data.json?rn=041202582')
+
+# assert print(r) == '<Response [200]>'
+
+# assert 'state' in json_data
+print(r.json())
+
+testString = str(r.json())
+
+assert 'zip' in testString
