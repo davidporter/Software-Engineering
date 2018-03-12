24153fbfafa0fdd07c602ab2851757866014da00
David Porter <davidbrianporter@gmail.com>
2018-03-12 10:27:20 -0400

    report created

--- HW2/test_coffee_amazon.py
+++ HW2/test_coffee_amazon.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-https://stackoverflow.com/questions/29858752/error-message-chromedriver-executable-needs-to-be-available-in-the-path
+# https://stackoverflow.com/questions/29858752/error-message-chromedriver-executable-needs-to-be-available-in-the-path
 
 browser = None
 
@@ -12,7 +12,7 @@ def setup_module(module):
     browser.get("http://www.amazon.com")
 
 def teardown_module(module):
-    time.sleep(10)
+    time.sleep(2)
     if browser:
         browser.close()
 
@@ -25,7 +25,7 @@ def test_is_cuisinart_a_displayed_coffee_maker():
     searchbox.clear()
     searchbox.send_keys("coffee maker")
     searchbox.send_keys(Keys.RETURN)
-    time.sleep(10)
+    time.sleep(2)
     id="s-results-list-atf"
     result_list = browser.find_element_by_id(id)
     result_text = result_list.text
